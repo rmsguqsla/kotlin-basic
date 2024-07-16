f053b9450aa0e81304c54f20f16bcfbdc1a9d72b
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2024-07-16 22:47:23 +0900

    연산자2

--- src/main/kotlin/operator/Operator.kt
+++ src/main/kotlin/operator/Operator.kt
@@ -1,7 +1,7 @@
 package operator
 
 fun main() {
-    // 1. 산술연산자
+    // 1. 산술 연산자
     // +, -, *, /, %
 //    val a: Int = 3
 //    val b: Int = 2
@@ -13,15 +13,51 @@ fun main() {
 //    println(a % b)
 
     // 2. 복합 대입 연산자
-    var a: Int = 11
-    a += 3
-    println(a)
-    a -= 3
-    println(a)
-    a *= 3
-    println(a)
-    a /= 3
-    println(a)
-    a %= 3
-    println(a)
+//    var a: Int = 11
+//    a += 3
+//    println(a)
+//    a -= 3
+//    println(a)
+//    a *= 3
+//    println(a)
+//    a /= 3
+//    println(a)
+//    a %= 3
+//    println(a)
+
+    // 3. 증감 연산자
+//    var value: Int = 10
+//    value++
+//    println("value = ${value}")
+//    value--
+//    println("value = ${value}")
+
+    // 4. 비교 연산자
+//    val a: Int = 3
+//    val b: Int = 3
+//    println(a < b)
+//    println(a <= b)
+//    println(a > b)
+//    println(a >= b)
+//    println(a == b)
+//    println(a != b)
+
+    // 5. 논리 연산자
+    // OR 연산자
+//    println(true || true)
+//    println(true || false)
+//    println(false || true)
+//    println(false || false)
+
+    // AND 연산자
+//    println(true && true)
+//    println(true && false)
+//    println(false && true)
+//    println(false && false)
+
+    // NOT 연산자
+//    println(!true)
+//    println(!false)
+
+
 }

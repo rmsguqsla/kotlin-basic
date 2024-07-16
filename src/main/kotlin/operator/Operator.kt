41a1891932e6efbbc50600eddfacf406d659483a
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2024-07-16 22:28:15 +0900

    연산자1

---
+++ src/main/kotlin/operator/Operator.kt
@@ -0,0 +1,27 @@
+package operator
+
+fun main() {
+    // 1. 산술연산자
+    // +, -, *, /, %
+//    val a: Int = 3
+//    val b: Int = 2
+//
+//    println(a + b)
+//    println(a - b)
+//    println(a * b)
+//    println(a / b)
+//    println(a % b)
+
+    // 2. 복합 대입 연산자
+    var a: Int = 11
+    a += 3
+    println(a)
+    a -= 3
+    println(a)
+    a *= 3
+    println(a)
+    a /= 3
+    println(a)
+    a %= 3
+    println(a)
+}

285ae609dc1b854515f1ebc8f270f8181d9c7d12
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2024-07-16 22:19:43 +0900

    변수 연습

---
+++ src/main/kotlin/variable/Practice1.kt
@@ -0,0 +1,22 @@
+package variable
+
+
+fun main() {
+    /**
+     * 자신 이름, 나이, 이메일주소를 변수로 선언하고 출력
+     */
+//    val name: String = "오근협"
+//    val age: Int = 27
+//    val email: String = "rmsguqsla@gmail.com"
+//
+//    println("이름은 ${name}이고 나이는 ${age}살이고 이메일주소는 ${email}입니다.")
+
+    /**
+     * 적절한 변수 타입을 넣어주세요.
+     */
+    val age: Int = 10
+    val pi: Double = 3.1415
+    val isPossible: Boolean = true
+    val constructionCosts: Long = 5_000_000_000
+    val name: String = "오근협"
+}
